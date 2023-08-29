80134254dad693abcd54fa6a621ab3dbf0585edb
Subhadeep-coder <dharsubhadeep5@gmail.com>
2023-08-29 22:39:00 +0530

    modified:   grouplancer/src/Components/Login.jsx

--- grouplancer/src/Components/Login.jsx
+++ grouplancer/src/Components/Login.jsx
@@ -1,31 +1,66 @@
-import React from "react";
+import React, { useState, useNavigate } from "react";
 
 
 const Login = () => {
 
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  let navigate = useNavigate();
+
+  //On submit of form
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    //API call 
+    const response = await fetch("http://localhost:8080/api/auth/login", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: credentials.email, password: credentials.password }), // body data type must match "Content-Type" header
+    });
+    const json = await response.json();
+    console.log(json);
+
+    if (json.authtoken) {
+      localStorage.setItem('token', json.authtoken);
+      navigate('/');
+    }
+    else {
+      // alert("Invalid Credentials!!");
+    }
+  }
+
+  //On change function
+  const onchange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
   return (
     <>
       <div className="container my-3">
-        <form>
+        <form onSubmit={handlesubmit}>
           <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">
+            <label htmlFor="email" className="form-label">
               Email address
             </label>
             <input
               type="email"
               className="form-control"
-              id="exampleInputEmail1"
+              id="email"
               aria-describedby="emailHelp"
+              required
+              onChange={onchange}
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">
+            <label htmlFor="password" className="form-label">
               Password
             </label>
             <input
               type="password"
               className="form-control"
-              id="exampleInputPassword1"
+              id="password"
+              required
+              onChange={onchange}
             />
           </div>
           <button type="submit" className="btn btn-primary">
