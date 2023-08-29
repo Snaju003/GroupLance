ee26b0089ecb81ae09786a20f18f04f43d782b53
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2023-08-29 21:55:06 +0530

    done

--- grouplancer/src/Components/Login.jsx
+++ grouplancer/src/Components/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
     <>
       <div className="container">
         <form>
-          
+
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">
               Email address
@@ -16,9 +16,7 @@ const Login = () => {
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
             />
-            <div id="emailHelp" className="form-text">
-              We'll never share your email with anyone else.
-            </div>
+            
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">
@@ -41,7 +39,7 @@ const Login = () => {
             </label>
           </div>
           <button type="submit" className="btn btn-primary">
-            Submit
+            Login
           </button>
         </form>
       </div>
--- grouplancer/src/Components/Navbar.jsx
+++ grouplancer/src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary mx-2">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">Grouplancer</Link>
+                    
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -34,7 +34,7 @@ const Navbar = () => {
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
-                        <Link to="/signup"><button className="btn btn-outline-success mx-2" type="submit">Signin</button></Link>
+                        <Link to="/signup"><button className="btn btn-outline-success mx-2" type="submit">Signup</button></Link>
                         <Link to="/login"><button className="btn btn-outline-success mx-2" type="submit">Login</button></Link>
                         <button className="btn btn-outline-success mx-2 disabled" type="submit">Logout</button>
                     </div>
