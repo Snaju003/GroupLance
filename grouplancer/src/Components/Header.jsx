0c028b6e855a987a2952638760001000f0c9fe91
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2023-08-29 21:43:43 +0530

    Done

--- grouplancer/src/Components/Header.jsx
+++ grouplancer/src/Components/Header.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <>
-    <nav className="navbar bg-light">
-  <div className="container">
-    <a className="navbar-brand" href="#">
-      <img src="./grouplance-logo.png" alt="Bootstrap" width="200" height="100"/>
-    </a>
-  </div>
-</nav>
+      <header>
+        <div className="container" style={{ justifyContent: "center" }}>
+          <a className="navbar-brand" href="#">
+            <img
+              src="./grouplance-logo.png"
+              alt="Bootstrap"
+              width="200"
+              height="150"
+            />
+          </a>
+        </div>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
