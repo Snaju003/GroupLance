a707266c2bb97e8834d20d5c2b35bf4299764621
Subhadeep-coder <dharsubhadeep5@gmail.com>
2023-08-30 16:02:14 +0530

    modified:   grouplancer/src/App.css 	new file:   grouplancer/src/Components/About.jsx 	modified:   grouplancer/src/Components/Home.jsx

--- grouplancer/src/Components/Home.jsx
+++ grouplancer/src/Components/Home.jsx
@@ -1,38 +1,14 @@
 import React from 'react';
+import About from './About';
 
 const Home = () => {
-  const cardcolor = "#dfdffb"
+  const cardcolor = "#dfdffb";
   return (
-    <div style={{backgroundColor:"#353595"}}>
+    <div style={{ backgroundColor: "#353595" }}>
       <div id="frontimage">
         <button type='submit' id='community'>Join the community</button>
       </div>
-      <h1 id='head1'>Why to use Grouplancer?</h1>
-      <section>
-        <article id="card1" style={{backgroundColor: cardcolor}}>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ut at vitae rem tenetur praesentium eveniet velit! Ad, dignissimos commodi quam quasi, assumenda provident tempora repellat velit similique incidunt aperiam.</p>
-        </article>
-        <article id="card2" style={{backgroundColor: cardcolor}}>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ut at vitae rem tenetur praesentium eveniet velit! Ad, dignissimos commodi quam quasi, assumenda provident tempora repellat velit similique incidunt aperiam.</p>
-        </article>
-        <article id="card3" style={{backgroundColor: cardcolor}}>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ut at vitae rem tenetur praesentium eveniet velit! Ad, dignissimos commodi quam quasi, assumenda provident tempora repellat velit similique incidunt aperiam.</p>
-        </article>
-      </section>
-      <br/>
-      <h1 id='head2'>How to use Grouplancer?</h1>
-      <section>
-        <article id="card4" style={{backgroundColor: cardcolor}}>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ut at vitae rem tenetur praesentium eveniet velit! Ad, dignissimos commodi quam quasi, assumenda provident tempora repellat velit similique incidunt aperiam.</p>
-        </article>
-        <article id="card5" style={{backgroundColor: cardcolor}}>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ut at vitae rem tenetur praesentium eveniet velit! Ad, dignissimos commodi quam quasi, assumenda provident tempora repellat velit similique incidunt aperiam.</p>
-        </article>
-        <article id="card6" style={{backgroundColor: cardcolor}}>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ut at vitae rem tenetur praesentium eveniet velit! Ad, dignissimos commodi quam quasi, assumenda provident tempora repellat velit similique incidunt aperiam.</p>
-        </article>
-      </section>
-      <br/>
+      <About cardcolor={cardcolor} />
     </div>
 
   )
