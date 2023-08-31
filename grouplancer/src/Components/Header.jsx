5d8a6a7d7feb21349c56d3af4bfb385cbf118820
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2023-09-01 01:17:03 +0530

    renamed:    grouplancer/public/grouplance-logo__2_-removebg-preview.png -> grouplancer/public/grouplan.png 	modified:   grouplancer/src/Components/Header.jsx

--- grouplancer/src/Components/Header.jsx
+++ grouplancer/src/Components/Header.jsx
@@ -5,11 +5,11 @@ const Header = (props) => {
     <>
       <header>
         <div className="container" style={{ display: "table" }}>
-          {/* <img
-            src="./grouplance-logo-fotor-bg-remover-20230829212146.png"
+          <img
+            src="./grouplan.png"
             alt="Logo"
             height="50"
-          /> */}
+          />
           <p style={{ fontSize: "40px", color: "white", textAlign: "center", verticalAlign: "middle", display: "table-cell", fontFamily: "engravers mt", padding: "10px" }}>GROUPLANCE</p>
           <p style={{ color: "white" }}>hello</p>
         </div>
