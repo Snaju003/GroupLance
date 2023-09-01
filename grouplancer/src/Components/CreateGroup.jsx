067d812a307a981331a94a87991720a44a7bd247
Snaju003 <soumyaraj2003@gmail.com>
2023-09-02 00:44:11 +0530

    modified:   grouplancer/src/Components/CreateGroup.jsx

--- grouplancer/src/Components/CreateGroup.jsx
+++ grouplancer/src/Components/CreateGroup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const CreateGroup = () => {
-  const [data,setData] = useState(0);
+  const [data,setData] = useState(1);
   return (
     <>
       <h1 className='text-center my-4' style={{ color: '#ffff' }}>Create Group</h1>
