9d579e447bcfb601c8a72edcc313d0471c29ca49
Subhadeep-coder <dharsubhadeep5@gmail.com>
2023-09-01 23:03:37 +0530

    modified:   grouplancer/src/Components/Home.jsx

--- grouplancer/src/Components/Home.jsx
+++ grouplancer/src/Components/Home.jsx
@@ -7,12 +7,8 @@ const Home = (props) => {
   return (
     <div style={{ backgroundColor: bgcolor }}>
       <div id="frontimage">
-        <img src="./grouplan2.png" alt="pictu" style={{ zIndex: 3 }} />
-        <p style={{ color: "white", width: "400px", marginLeft: "100px", paddingTop: "400px", textAlign: "justify" }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat autem accusamus doloribus beatae accusantium. Ea molestias doloremque aspernatur numquam explicabo ipsa excepturi accusantium. Sapiente fuga doloremque sit totam architecto incidunt!</p>
-        {/* <img src="./grouplace-logo_3.png" style={{zIndex: "1"}}/> */}
-        <h1 style={{ color: "white", paddingTop: "100px", marginLeft: "100px", }}>Grouplance</h1>
         <img src="./grouplan2.png" alt="pictu" />
-        <p style={{ color: "white", width: "400px", marginLeft: "100px", paddingTop: "400px", textAlign: "justify" }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat autem accusamus doloribus beatae accusantium. Ea molestias doloremque aspernatur numquam explicabo ipsa excepturi accusantium. Sapiente fuga doloremque sit totam architecto incidunt!</p>
+        <p style={{color: "white", width: "400px", marginLeft: "100px", paddingTop: "400px", textAlign: "justify"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat autem accusamus doloribus beatae accusantium. Ea molestias doloremque aspernatur numquam explicabo ipsa excepturi accusantium. Sapiente fuga doloremque sit totam architecto incidunt!</p>
         <button type='submit' id='community'>Join the community</button>
       </div>
       <div id='cards'>
