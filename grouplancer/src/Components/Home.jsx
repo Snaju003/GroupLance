1b0fc766babba046c009b858d0ec22de721e8d7e
PratyushPal29 <29palpratyush04@gmail.com>
2023-09-01 22:50:14 +0530

    modified:   grouplancer/src/Components/Home.jsx

--- grouplancer/src/Components/Home.jsx
+++ grouplancer/src/Components/Home.jsx
@@ -7,12 +7,8 @@ const Home = (props) => {
   return (
     <div style={{ backgroundColor: bgcolor }}>
       <div id="frontimage">
-<<<<<<< HEAD
         {/* <img src="./grouplace-logo_3.png" style={{zIndex: "1"}}/> */}
         <h1 style={{color: "white", paddingTop: "100px", marginLeft: "100px", }}>Grouplance</h1>
-=======
-        <img src="./grouplan2.png" alt="pictu" />
->>>>>>> fc65963914146cf8c94261d13fc2092f80d29a20
         <p style={{color: "white", width: "400px", marginLeft: "100px", paddingTop: "400px", textAlign: "justify"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat autem accusamus doloribus beatae accusantium. Ea molestias doloremque aspernatur numquam explicabo ipsa excepturi accusantium. Sapiente fuga doloremque sit totam architecto incidunt!</p>
         <button type='submit' id='community'>Join the community</button>
       </div>
