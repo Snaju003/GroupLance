084a4adec57a25ace492d30e4cb7148f7fc7780c
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2023-09-03 00:16:37 +0530

    modified:   grouplancer/src/Components/Home.jsx

--- grouplancer/src/Components/Home.jsx
+++ grouplancer/src/Components/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import About from './About';
+import React from "react";
+import About from "./About";
 
 const Home = (props) => {
   const { bgcolor } = props;
@@ -7,18 +7,45 @@ const Home = (props) => {
   return (
     <div style={{ backgroundColor: bgcolor }}>
       <div id="frontimage">
-        <p style={{width: "400px", color: "white", fontSize: "35px", marginLeft: "100px", paddingTop: "150px", marginBottom: "-180px", fontWeight: "bold"}}>Hello, this is a freelancing website</p>
-        <p style={{ color: "white", width: "400px", marginLeft: "100px", paddingTop: "200px", textAlign: "justify" }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat autem accusamus doloribus beatae accusantium. Ea molestias doloremque aspernatur numquam explicabo ipsa excepturi accusantium. Sapiente fuga doloremque sit totam architecto incidunt!</p>
-        <button type='submit' id='community'>Join the community</button>
+        <p
+          style={{
+            width: "400px",
+            color: "white",
+            fontSize: "30px",
+            marginLeft: "100px",
+            paddingTop: "120px",
+            marginBottom: "-180px",
+            fontWeight: "bold",
+            fontallign: "center",
+          }}
+        >
+          Find and Connect with like-minded individuals and create or join groups effortlessly, online.
+        </p>
+        <p
+          style={{
+            color: "white",
+            width: "400px",
+            marginLeft: "100px",
+            paddingTop: "200px",
+            textAlign: "justify",
+          }}
+        >
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          autem accusamus doloribus beatae accusantium. Ea molestias doloremque
+          aspernatur numquam explicabo ipsa excepturi accusantium. Sapiente fuga
+          doloremque sit totam architecto incidunt!
+        </p>
+        <button type="submit" id="community">
+          Join the community
+        </button>
       </div>
-      <div id='cards'>
+      <div id="cards">
         <br></br>
         <About cardcolor={cardcolor} />
         <br></br>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
