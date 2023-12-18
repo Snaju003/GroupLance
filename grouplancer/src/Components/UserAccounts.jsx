a9a45b4f0b8cf799ac6b6cf2ef6ba4a763b290b1
PratyushPal29 <29palpratyush04@gmail.com>
2023-12-18 11:40:19 +0530

    modified:   grouplancer/src/Components/UserAccounts.jsx

--- grouplancer/src/Components/UserAccounts.jsx
+++ grouplancer/src/Components/UserAccounts.jsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const UserAccounts = () => {
+const groupDesign = {
+    backgroundColor: "lightgray", marginLeft: "20px", padding: "15px", borderRadius: "15px"
+}
+
+const UserAccounts = ({ userId }) => {
+
+    const [userData, setUserData] = useState({ name: "", email: "" });
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await fetch(`http://localhost:8080/api/auth/getuser/${userId}`);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+                setUserData(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchUserData();
+    }, [userId]);
 
     return (
         <>
-            <div className="container" style={{display: "flex", marginTop: "30px"}}>
-                <img 
+            <div className="container" style={{ display: "flex", marginTop: "30px" }}>
+                <img
                     src="./default-user.jpg"
                     alt="profile_image"
-                    style={{marginRight: "350px", marginLeft: "30px", height: "200px", width: "200px"}}
+                    style={{ marginRight: "350px", marginLeft: "30px", height: "200px", width: "200px" }}
                 />
-                <div style={{marginTop: "30px", backgroundColor: "white", width: "1500px", textAlign: "justify", borderRadius: "15px", paddingLeft: "15px", paddingTop: "10px"}}>
-                    <p style={{fontSize: "25px"}}>Name: </p>
-                    <p style={{fontSize: "25px"}}>Email: </p>
-                    <p style={{fontSize: "25px"}}>Joined Groups: </p>
+                <div style={{ marginTop: "30px", backgroundColor: "white", width: "1500px", textAlign: "justify", borderRadius: "15px", paddingLeft: "15px", paddingTop: "10px" }}>
+                    <p style={{ fontSize: "25px" }}>Name: {userData.name}</p>
+                    <p style={{ fontSize: "25px" }}>Email: {userData.email}</p>
+                    <p style={{ fontSize: "25px" }}>Joined Groups: </p>
                 </div>
             </div>
-            <div className= "group-container" style={{backgroundColor: "white", marginLeft: "120px", marginRight: "110px", marginTop: "40px", padding: "15px", borderRadius: "15px"}}>
-                <p style={{fontSize: "25px"}}>Group Information</p>
-                <div className="sub-group-container" style={{backgroundColor: "lightgray", marginLeft: "20px", padding: "15px", borderRadius: "15px"}}>
+            <div className="group-container" style={{ backgroundColor: "white", marginLeft: "120px", marginRight: "110px", marginTop: "40px", padding: "15px", borderRadius: "15px" }}>
+                <p style={{ fontSize: "25px" }}>Group Information</p>
+                <div className="sub-group-container" style={{ groupDesign }}>
                     <p>Group 1</p>
                 </div>
-                <div className="sub-group-container" style={{backgroundColor: "lightgray", marginLeft: "20px", padding: "15px", borderRadius: "15px", marginTop: "10px"}}>
+                <div className="sub-group-container" style={{ groupDesign }}>
                     <p>Group 2</p>
                 </div>
             </div>
