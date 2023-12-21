a41bc06bcf35f2159f708142ee3f1197383b2dad
shreedatri <shreedatrisaha@gmail.com>
2023-12-22 02:16:37 +0530

    modified:   grouplancer/src/Components/Home.jsx

--- grouplancer/src/Components/Home.jsx
+++ grouplancer/src/Components/Home.jsx
@@ -61,6 +61,54 @@ const Home = (props) => {
         <About cardcolor={cardcolor} />
         <br></br>
       </div>
+      <div class="row">
+        <div class="col-sm-4" style={{paddingLeft:"250px",alignItems:"center"}}>
+          <div class="card" style={{backgroundImage: "linear-gradient(to right , #b2d3c2, #b0fc38", width:"300px",height:"400px",borerRadius:"20px",backgroundColor:"white",borderColor:"green",boxShadow:"0 0 5px 5px"}}>
+            <h3 style={{paddingLeft:"118px"}}>Free</h3>
+            <ui style={{marginBottom:"150px",marginTop:"30px"}} >
+              <li style={{}}>Ad free</li>
+              <li>Create only one group</li>
+              <li>Create groups of only specified size</li>
+             
+            </ui>
+          <button type="button" class="btn btn-outline-primary"  style={{color:"white",backgroundColor:"green",width:"140px",height:"50px",borderRadius:"10px",marginTop:"15px",marginLeft:"70px"}}>free</button>
+          </div>
+          <div class="card-body">
+            
+          </div>
+        </div>
+        <div class="col-sm-4" style={{paddingLeft:"120px",alignItems:"center"}}>
+          <div class="card" style={{backgroundImage: "linear-gradient(to right , #63c5da, #ffff", width:"300px",height:"400px",borerRadius:"20px",boxShadow:"0 0 5px 5px"}}>
+            <h3 style={{paddingLeft:"85px"}}>Premium</h3>
+            <ui style={{marginBottom:"150px",marginTop:"30px"}} >
+              <li style={{listStyle:"inherit"}}>Ad free</li>
+              <li>Create multiple groups</li>
+              <li>Create groups of any size</li>
+              
+            </ui>
+          <button type="button"  class="btn btn-outline-primary"  style={{color:"white",backgroundColor:bgcolor,width:"140px",height:"50px",borderRadius:"10px",marginTop:"45px",marginLeft:"70px"}}>premiun</button>
+          </div>
+          <div class="card-body">
+            
+          </div>
+        </div>
+        <div class="col-sm-4" style={{paddingRight:"250px",alignItems:"center"}}>
+          <div class="card" style={{  backgroundImage: "linear-gradient(to right , #9a4eae,#ffff",width:"300px",height:"400px",borerRadius:"20px",boxShadow:"0 0 5px 5px"}}>
+            <h3 style={{paddingLeft:"98px"}}>Deluxe</h3>
+            <ui style={{marginBottom:"150px",marginTop:"30px"}} >
+              <li style={{listStyle:"inherit"}}>Ad free</li>
+              <li>Create multiple groups</li>
+              <li>Create groups of any size</li>
+              <li>Invite top users to your group</li>
+              <li>Send requests to top users</li>
+            </ui>
+          <button type="button" class="btn btn-outline-primary" style={{color:"white",backgroundColor:"purple",width:"140px",height:"50px",borderRadius:"10px",marginTop:"0px",marginLeft:"70px",justifyContent:"center"}}>deluxe</button>
+          </div>
+          <div class="card-body">
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
