44dac5d3b68a3cfe885d5ed277061b5c1a010bee
PratyushPal29 <29palpratyush04@gmail.com>
2023-12-22 00:56:38 +0530

    modified:   grouplancer/src/Components/LiveGroup.jsx

--- grouplancer/src/Components/LiveGroup.jsx
+++ grouplancer/src/Components/LiveGroup.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import { useUser } from "../context/UserContext";
 
 const LiveGroup = (props) => {
     let { title, description, color } = props;
+    const {currentUser} = useUser()
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:8080/api/group/join-group", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token": localStorage.getItem('auth-token'),
+          },
+          body: JSON.stringify({ userId: currentUser, groupId: e.target.id }),
+        });
+        const json = await response.json();
+
+    }
     return (
         <>
             <div className="card" style={{ backgroundColor: color }}>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href="/" target='__blank' className="btn btn-primary">Join Group</a>
+                    <a href="/" className="btn btn-primary" onClick={handleSubmit}>Join Group</a>
                 </div>
             </div>
         </>
