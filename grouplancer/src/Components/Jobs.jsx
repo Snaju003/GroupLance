097f2f0bff342bcc6e94f2e4902b5233a953a615
shreedatri <shreedatrisaha@gmail.com>
2023-12-24 01:14:20 +0530

    new file:   grouplancer/public/app.png 	new file:   grouplancer/public/cyber.jpg 	new file:   grouplancer/public/general.jpg 	new file:   grouplancer/public/iot.jpg 	new file:   grouplancer/public/web.png 	modified:   grouplancer/src/Components/Jobs.jsx

--- grouplancer/src/Components/Jobs.jsx
+++ grouplancer/src/Components/Jobs.jsx
@@ -5,19 +5,20 @@ import React from 'react'
 const Jobs = () => {
   return (
     <>
-      <div class="nav-down" style={{ marginLeft: "2rem" }} >
+      <div class="nav-down" style={{borderRadius:"20px", marginLeft: "2rem",marginRight:"2rem"}} >
         <div><p style={{ color: "white", marginTop: "3rem" }}>Freelancer . Jobs
         </p>
         </div>
-        <h1 style={{ color: "white", marginBottom: "1rem" }}>
+        <h1 style={{ color: "white", marginBottom: "1rem" ,marginLeft:"500px" }}>
           Browse by Category
         </h1>
-        <input type="search" className="category_search" placeholder="Search for a category" color="white" style={{ border: "none", width: "80%", height: "2.5rem", borderRadius: "4px" }} />
+        <input type="search" className="category_search" placeholder="Search for a category" color="white" style={{ border: "none", width: "80%", height: "2.5rem", borderRadius: "4px",marginLeft:"90px" }} />
         <button class="btn btn-outline-success" type="submit" color='white' style={{height:"2.7rem"}}>Search</button>
       </div>
-      <div className="box" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-evenly", marginLeft: "2rem", marginRight: "2rem", marginTop: "4rem", color: "white" }}>
+      <div className="box" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-evenly", marginLeft: "2rem", marginRight: "2rem", color: "white",backgroundImage: "linear-gradient( to bottom , #012a4a,#080402 ",paddingTop:"20px",borderRadius:"20px" }}>
         <div className="box1" style={{ color: "white", width: "30%", height: "6.5rem", display: "flex", flexDirection: "row" }}>
-          <div className="icon" style={{ backgroundColor: "white", color: "black", paddingRight: "1rem" }}>Icon </div>
+          
+          < img src="./general.jpg" alt="icon" style={{height:"80px",width:"80px",marginLeft:"50px"}} />
 
           <div className="box-container" style={{ display: "flex", flexDirection: "column", marginLeft: "1.5rem" }}>
             <h5 style={{ marginLeft: "1rem", display: "inline", marginLeft: "0px" }}> General</h5>
@@ -25,28 +26,28 @@ const Jobs = () => {
           </div>
         </div>
         <div className="box2" style={{ color: "white", width: "30%", height: "6.5rem", display: "flex", flexDirection: "row", marginLeft: "1rem" }}>
-          <div className="icon" style={{ backgroundColor: "white", color: "black", paddingRight: "1rem" }}>Icon </div>
+        < img src="./web.png" alt="icon" style={{height:"80px",width:"80px",marginLeft:"50px"}} />
 
           <div className="box-container" style={{ display: "flex", flexDirection: "column", marginLeft: "1.5rem" }}>
             <h5 style={{ marginLeft: "1rem", display: "inline", marginLeft: "0px" }}> Web Development</h5>
             <a href="">Let's make a website together...</a>
           </div>
         </div> <div className="box3" style={{ color: "white", width: "30%", height: "6.5rem", display: "flex", flexDirection: "row", marginLeft: "1rem" }}>
-          <div className="icon" style={{ backgroundColor: "white", color: "black", paddingRight: "1rem" }}>Icon </div>
+        < img src="./app.png" alt="icon" style={{height:"80px",width:"80px",marginLeft:"50px"}} />
 
           <div className="box-container" style={{ display: "flex", flexDirection: "column", marginLeft: "1.5rem" }}>
             <h5 style={{ marginLeft: "1rem", display: "inline", marginLeft: "0px" }}>App Development</h5>
             <a href="">Let's develop a mobile application together...</a>
           </div>
         </div> <div className="box4" style={{ color: "white", width: "30%", height: "6.5rem", display: "flex", flexDirection: "row", marginTop: "2rem" }}>
-          <div className="icon" style={{ backgroundColor: "white", color: "black", paddingRight: "1rem" }}>Icon </div>
+        < img src="./cyber.jpg" alt="icon" style={{height:"80px",width:"80px",marginLeft:"50px"}} />
 
           <div className="box-container" style={{ display: "flex", flexDirection: "column", marginLeft: "1.5rem" }}>
             <h5 style={{ marginLeft: "1rem", display: "inline", marginLeft: "0px" }}> CyberSecurity</h5>
             <a href=""> CyberSecurity...</a>
           </div>
         </div> <div className="box5" style={{ color: "white", width: "30%", height: "6.5rem", display: "flex", flexDirection: "row", marginLeft: "1rem", marginTop: "2rem" }}>
-          <div className="icon" style={{ backgroundColor: "white", color: "black", paddingRight: "1rem" }}>Icon </div>
+        < img src="./iot.jpg" alt="icon" style={{height:"80px",width:"80px",marginLeft:"50px"}} />
 
           <div className="box-container" style={{ display: "flex", flexDirection: "column", marginLeft: "1.5rem" }}>
             <h5 style={{ marginLeft: "1rem", display: "inline", marginLeft: "0px" }}> IOT</h5>
