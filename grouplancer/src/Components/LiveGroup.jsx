d076b540b93336c509d8116f97026fcca12f7528
PratyushPal29 <29palpratyush04@gmail.com>
2023-12-25 01:49:46 +0530

    deleted:    grouplancer/habibi.html 	modified:   grouplancer/src/Components/LiveGroup.jsx 	modified:   grouplancer/src/Components/LiveGroups.jsx

--- grouplancer/src/Components/LiveGroup.jsx
+++ grouplancer/src/Components/LiveGroup.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { useUser } from "../context/UserContext";
 
-const LiveGroup = (props) => {
-    let { title, description, color } = props;
-    const {currentUser} = useUser()
-    const handleSubmit = async (e) => {
+const LiveGroup = ({color,title,description,id,canJoin}) => {
+    const liveGroup = async (e) => {
         e.preventDefault();
         const response = await fetch("http://localhost:8080/api/group/join-group", {
           method: "PUT",
@@ -12,10 +10,10 @@ const LiveGroup = (props) => {
             "Content-Type": "application/json",
             "auth-token": localStorage.getItem('auth-token'),
           },
-          body: JSON.stringify({ userId: currentUser, groupId: e.target.id }),
+          body: JSON.stringify({ groupId: id }),
         });
         const json = await response.json();
-
+        console.log(json)
     }
     return (
         <>
@@ -23,7 +21,7 @@ const LiveGroup = (props) => {
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href="/" className="btn btn-primary" onClick={handleSubmit}>Join Group</a>
+                    <a href="/" className="btn btn-primary" onClick={liveGroup}>Join Group</a>
                 </div>
             </div>
         </>
