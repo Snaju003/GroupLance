a322d2e7283be62d7fab93c3b196d9257f9b6d9f
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2023-12-30 01:21:56 +0530

    modified:   grouplancer/src/Components/group/joined_groups/JoinedGroups.jsx 	modified:   grouplancer/src/Components/group/live_groups/LiveGroup.jsx 	modified:   grouplancer/src/Components/group/live_groups/LiveGroups.jsx

--- grouplancer/src/Components/group/live_groups/LiveGroups.jsx
+++ grouplancer/src/Components/group/live_groups/LiveGroups.jsx
@@ -1,61 +1,70 @@
-import React, { useState, useEffect } from 'react'
-import LiveGroup from './LiveGroup';
+import React, { useState, useEffect } from "react";
+import LiveGroup from "./LiveGroup";
 import { useUser } from "../../../context/UserContext";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const LiveGroups = () => {
-    const color = "#dfdffb";
-    const [liveGroupData, setLiveGroupData] = useState([]);
-    const navigate = useNavigate()
-    const { currentUser } = useUser();
-    useEffect(() => {
-        if (!currentUser)
-            navigate("/login")
-        else {
-            const getAllGroups = async () => {
-                try {
-                    const authToken = localStorage.getItem("auth-token");
-                    const response = await fetch(
-                        `http://localhost:8080/api/group/get-all-groups`,
-                        {
-                            method: "GET",
-                            headers: {
-                                "Content-Type": "application/json",
-                                "auth-token": authToken,
-                            },
-                        }
-                    );
-                    const data = await response.json();
-                    const filteredGroups = data.groups.filter(group => group.leader !== currentUser._id);
-                    //console.log(filteredGroups)
-                    setLiveGroupData(filteredGroups);
-                } catch (error) {
-                    console.error(error);
-                }
+  const color = "#dfdffb";
+  const [liveGroupData, setLiveGroupData] = useState([]);
+  const navigate = useNavigate();
+  const { currentUser } = useUser();
+  useEffect(() => {
+    if (!currentUser) navigate("/login");
+    else {
+      const getAllGroups = async () => {
+        try {
+          const authToken = localStorage.getItem("auth-token");
+          const response = await fetch(
+            `http://localhost:8080/api/group/get-all-groups`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                "auth-token": authToken,
+              },
             }
-            getAllGroups()
+          );
+          const data = await response.json();
+          const filteredGroups = data.groups.filter(
+            (group) => group.leader !== currentUser._id
+          );
+          //console.log(filteredGroups);
+          setLiveGroupData(filteredGroups);
+        } catch (error) {
+          console.error(error);
         }
-    }, [currentUser, navigate])
-    return (
-        <>
-            <h1 className='text-center my-4' style={{ color: '#ffff' }}>Live Groups</h1>
-            <div className="container">
-                <div className="container row">
-                    {
-                        liveGroupData.map(({ _id, gName, gDesc, anyoneCanJoin }) => {
-                            return (
-                                <div className="col-md-3 mb-3" key={_id}>
-                                    <LiveGroup id={_id} title={gName} description={gDesc} canJoin={anyoneCanJoin} color={color} />
-                                </div>
-                            )
-                        }
-
-                        )
-                    }
+      };
+      getAllGroups();
+    }
+  }, [currentUser, navigate]);
+  return (
+    <>
+      <h1 className="text-center my-4" style={{ color: "#ffff" }}>
+        Live Groups
+      </h1>
+      <div className="container">
+        <div className="container row">
+          {liveGroupData.map(
+            ({ _id, gName, goal, projName, anyoneCanJoin }) => {
+              return (
+                <div className="col-md-3 mb-3" key={_id}>
+                  <LiveGroup
+                  
+                    id={_id}
+                    title={gName}
+                    projName={projName}
+                    mainGoal={goal}
+                    canJoin={anyoneCanJoin}
+                    color={color}
+                  />
                 </div>
-            </div>
-        </>
-    )
-}
+              );
+            }
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default LiveGroups;
