4d8b717b00b2d9d1627c53fc60e2142c5dc8e757
PratyushPal29 <29palpratyush04@gmail.com>
2024-01-04 01:44:19 +0530

    modified:   grouplancer/src/Components/sidebar.jsx

--- grouplancer/src/Components/sidebar.jsx
+++ grouplancer/src/Components/sidebar.jsx
@@ -6,70 +6,9 @@ import GroupsInvite from "./group/group_invite/groupsinvite";
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 
 const Sidebar = () => {
-    const [activeTab, setActiveTab] = useState("myGroups");
-
-    const handleTabChange = (tab) => {
-        setActiveTab(tab);
-    };
 
     return (
         <>
-            {/* <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <div style={{ display: "flex" }}>
-                    <button
-                        className="button-48"
-                        style={{
-                            padding: "10px",
-                            marginRight: "10px",
-                            backgroundColor: activeTab === "myGroups" ? "#00b4d8" : "#ffffff",
-                            color: activeTab === "myGroups" ? "#ffffff" : "#000000",
-                        }}
-                        onClick={() => handleTabChange("myGroups")}
-                    ><span>
-                            My groups</span>
-                    </button>
-                    <button
-                        className="button-48"
-                        style={{
-                            padding: "10px",
-                            marginRight: "10px",
-                            backgroundColor: activeTab === "joinedGroups" ? "#00b4d8" : "#ffffff",
-                            color: activeTab === "joinedGroups" ? "#ffffff" : "#000000",
-                        }}
-                        onClick={() => handleTabChange("joinedGroups")}
-                    >
-                        <span>  Joined groups</span>
-                    </button>
-                    <button
-                        className="button-48"
-                        style={{
-
-                            padding: "10px",
-                            marginRight: "10px",
-                            backgroundColor: activeTab === "createGroup" ? "#00b4d8" : "#ffffff",
-                            color: activeTab === "createGroup" ? "#ffffff" : "#000000",
-                        }}
-                        onClick={() => handleTabChange("createGroup")}
-                    >
-                        <span> Create Group</span>
-                    </button>
-                    <button
-                        className="button-48"
-                        style={{
-                            padding: "10px",
-                            backgroundColor: activeTab === "groupInvites" ? "#00b4d8" : "#ffffff",
-                            color: activeTab === "groupInvites" ? "#ffffff" : "#000000",
-                        }}
-                        onClick={() => handleTabChange("groupInvites")}
-                    >
-                        <span> Group Invites</span>
-                    </button>
-                </div>
-                {activeTab === "myGroups" && <MyGroups />}
-                {activeTab === "joinedGroups" && <JoinedGroups />}
-                {activeTab === "createGroup" && <CreateGroup />}
-                {activeTab === "groupInvites" && <GroupsInvite />}
-            </div> */}
             <section className="tab">
                 <Container>
                     <Row>
