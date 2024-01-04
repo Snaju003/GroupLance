3f47d081148dde905d23dc65aa2f1311434a033b
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-01-05 01:53:25 +0530

    new file:   grouplancer/src/Components/ChatBox/Chat.css 	modified:   grouplancer/src/Components/ChatBox/Chat.jsx 	modified:   grouplancer/src/Components/ChatBox/ChatBox.jsx 	modified:   grouplancer/src/Components/ChatBox/ChatHeader.jsx 	modified:   grouplancer/src/Components/ChatBox/ContactList.jsx

--- grouplancer/src/Components/ChatBox/ContactList.jsx
+++ grouplancer/src/Components/ChatBox/ContactList.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
-const ContactList = () => {
-  return (
-    <>
-      <p style={{color:'#fff'}}>
-        Hey This is Contact List
-      </p>
-    </>
+
+  const ContactList = () => {
+    // Sample contacts
+    const contacts = [
+      { id: 1, name: 'Group1' },
+      { id: 2, name: 'Group2' },
+      { id: 3, name: 'Group3' },
+    ];
+  
+    return (
+      <>
+        <h1 className="text-center my-4" style={{ color: '#ffff' }}>
+          Contacts
+        </h1>
+        <ul className="list-group">
+          {contacts.map((contact) => (
+            <li key={contact.id} className="list-group-item">
+              <strong>{contact.name}</strong>
+              <br />
+              
+            </li>
+          ))}
+        </ul>
+      </>
   )
 }
 
