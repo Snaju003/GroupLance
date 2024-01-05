88805aad9fcacaed54aedf541f863085cbb3b1d5
PratyushPal29 <29palpratyush04@gmail.com>
2024-01-06 01:19:49 +0530

    modified:   grouplancer/src/Components/Posts/Post.jsx 	modified:   grouplancer/src/Components/Posts/Post_css.css

--- grouplancer/src/Components/Posts/Post.jsx
+++ grouplancer/src/Components/Posts/Post.jsx
@@ -5,7 +5,6 @@ const Post = () => {
     const [likes, setLikes] = useState(0);
     const [comments, setComments] = useState([]);
     const [commentInput, setCommentInput] = useState('');
-    const [showModal, setShowModal] = useState(false);
 
     const handleLike = () => {
         setLikes(likes + 1);
@@ -18,43 +17,45 @@ const Post = () => {
         }
     };
 
-    const closeModal = () => {
-        setShowModal(false);
-    };
-
     return (
         <div className="post-container">
-            <img src='./aboutUs.jpg' alt='posts' style={{
-                marginRight: "20px",
-                marginLeft: "20px",
-                height: "200px",
-                width: "200px",
-                marginTop: "10px",
-            }} />
-            <div className="comment-section">
-                <button className="like-button" onClick={handleLike}>
-                    Like ({likes})
-                </button>
-                <button className="comment-button" onClick={handleComment}>
-                    Comment
-                </button>
-                <input
-                    type="text"
-                    placeholder="Add a comment"
-                    value={commentInput}
-                    onChange={(e) => setCommentInput(e.target.value)}
-                    className="comment-input"
-                />
-            </div>
-            <div>
-                <h4 style={{ marginTop: "10px", color: "white" }}>Comments:</h4>
-                <ul className="comments-list">
-                    {comments.map((comment, index) => (
-                        <li key={index} className="comment-item" style={{ color: "white" }}>
-                            {comment}
-                        </li>
-                    ))}
-                </ul>
+            <div style={{ display: "flex" }}>
+                <div>
+                    <img src='./aboutUs.jpg' alt='posts' style={{
+                        marginRight: "20px",
+                        marginLeft: "20px",
+                        height: "200px",
+                        width: "200px",
+                        marginTop: "10px",
+                    }} />
+                    <button className="like-button" onClick={handleLike}>
+                        Like ({likes})
+                    </button>
+                </div>
+                <div className="comment-section" style={{ borderLeft: "3px solid white"}}>
+                    <div style={{display: "flex"}}>
+                        <input
+                            type="text"
+                            placeholder="Add a comment"
+                            value={commentInput}
+                            onChange={(e) => setCommentInput(e.target.value)}
+                            className="comment-input"
+                        />
+                        <button className="comment-button" onClick={handleComment}>
+                            Comment
+                        </button>
+                    </div>
+                    <div>
+                        <h4 style={{ marginTop: "10px", color: "white", marginLeft: "2vh", }}>Comments:</h4>
+                        <ul className="comments-list">
+                            {comments.map((comment, index) => (
+                                <li key={index} className="comment-item" style={{ color: "white" }}>
+                                    {comment}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     );
