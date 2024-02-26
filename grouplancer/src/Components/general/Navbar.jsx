5d96fa5a724b364712e739c9db57b38d8c9c1c13
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-02-27 00:42:05 +0530

    modified:   grouplancer/package-lock.json 	modified:   grouplancer/package.json 	modified:   grouplancer/src/Components/general/Navbar.jsx

--- grouplancer/src/Components/general/Navbar.jsx
+++ grouplancer/src/Components/general/Navbar.jsx
@@ -1,148 +1,240 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { useUser } from "../../context/UserContext";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { HashLink } from 'react-router-hash-link';
-import {
-    BrowserRouter as Router
-} from "react-router-dom";
+// import React, { useState, useEffect } from 'react'
+// import { Link } from 'react-router-dom'
+// import { useUser } from "../../context/UserContext";
+// import { Navbar, Nav, Container } from "react-bootstrap";
+// import { HashLink } from 'react-router-hash-link';
+// import {
+//     BrowserRouter as Router
+// } from "react-router-dom";
+
+// const NavBar = () => {
+//     const { currentUser } = useUser();
+//     const [activeLink, setActiveLink] = useState('home');
+//     const [scrolled, setScrolled] = useState(false);
+
+//     useEffect(() => {
+//         const onScroll = () => {
+//             if (window.scrollY > 50) {
+//                 setScrolled(true);
+//             } else {
+//                 setScrolled(false);
+//             }
+//         }
+
+//         window.addEventListener("scroll", onScroll);
+
+//         return () => window.removeEventListener("scroll", onScroll);
+//     }, [])
+
+//     const onUpdateActiveLink = (value) => {
+//         setActiveLink(value);
+//     }
+
+//     return (
+//         <>
+            
+           
+//             <Router>
+//                 <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+//                     <Container>
+//                         <Navbar.Brand href="/">
+//                             <img src="./grouplan.png" alt="Logo" style={{ width: "300px", height: "200px", justifyContent: "space-between", marginBottom: "-50px", marginTop: "-50px" }} />
+//                         </Navbar.Brand>
+//                         <Navbar.Toggle aria-controls="basic-navbar-nav">
+//                             <span className="navbar-toggler-icon"></span>
+//                         </Navbar.Toggle>
+//                         <Navbar.Collapse id="basic-navbar-nav">
+//                             <Nav className="ms-auto">
+//                                 <Link to="/" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Link>
+//                                 <Link to="sidebar" className={activeLink === 'groups' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('groups')}>Groups</Link>
+//                                 <Link to="liveside" className={activeLink === 'categories' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('categories')}>Categories</Link>
+//                                 <Link to="sidebar_ranking" className={activeLink === 'rankings' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('rankings')}>Rankings</Link>
+//                                 <Link to="chatbox" className={activeLink === 'chatbox' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('chatbox')}>ChatBox</Link>
+//                                 <Link to="aboutus" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>About Us</Link>
+//                             </Nav>
+//                             <span className="navbar-text">
+//                                 <div className="social-icon">
+//                                     <a href="#"><img src={navIcon1} alt="" /></a>
+//                                     <a href="#"><img src={navIcon2} alt="" /></a>
+//                                     <a href="#"><img src={navIcon3} alt="" /></a>
+//                                 </div>
+//                                 {
+//                                     currentUser ?
+
+//                                             <Nav.Link className="profile" href="userAccount"><button className="vvd"><span>Profile</span></button></Nav.Link>
+//                                              : <HashLink to='#connect'>
+//                                             <button className="vvd"><span>Sign Up/Sign In</span></button>
+//                                         </HashLink>
+//                                 }
+//                             </span>
+//                         </Navbar.Collapse>
+//                     </Container>
+//                 </Navbar>
+//             </Router>
+           
+
+//         </>
+//     )
+// }
+
+// export default NavBar
+import React, { useState, useEffect } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { HashLink } from "react-router-hash-link";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { Link } from "@mui/material";
+
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const NavBar = () => {
-    const { currentUser } = useUser();
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        }
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-        window.addEventListener("scroll", onScroll);
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-        return () => window.removeEventListener("scroll", onScroll);
-    }, [])
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
-    }
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <img src="log1.png" style={{ width: "15vh", paddingRight: "5vh" }} />
 
-    return (
-        <>
-            {/* <nav className="navbar navbar-expand-lg" data-bs-theme="dark" style={{ borderRadius: "20px", display: "flex", justifyContent: "space-between", backgroundImage: "linear-gradient(to top, #000066,   #4d4dff)", boxShadow: "7px 7px 7px rgba(0, 0, 0, 0.9)" }}>
-                <div className="container-fluid" style={{}}>
-
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="nav" id="navbarSupportedContent" style={{display: "flex", justifyContent:"center", margin: "auto"}}>
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={{marginRight: "0px"}}>
-                            <li className="nav-item mx-2">
-                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item dropdown mx-2">
-                                <Link className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Groups
-                                </Link>
-                                <ul className="dropdown-menu">
-                                    <li>
-                                        <Link className="dropdown-item" to="/Mygroups">My Groups</Link>
-                                    </li>
-                                    <li>
-                                        <Link className="dropdown-item" to="/joinedgroups">Joined Groups</Link>
-                                    </li>
-                                    <li>
-                                        <Link className="dropdown-item" to="/groupinvite">Group Invite</Link>
-                                    </li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li>
-                                        <Link className="dropdown-item" to="/creategroup">Create Group</Link>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li className="nav-item dropdown mx-2">
-                                <Link className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Catagories
-                                </Link>
-                                <ul className="dropdown-menu">
-                                    <li><Link className="dropdown-item" to="/findjob">Browse Domains</Link></li>
-                                    <li><Link className="dropdown-item" to="/livegroups">All Groups</Link></li>
-                                    
-                                </ul>
-                            </li>
-                            <li className="nav-item dropdown mx-2">
-                                <Link className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Rankings
-                                </Link>
-                                <ul className="dropdown-menu">
-                                    <li><Link className="dropdown-item" to="/topgroups">Groups</Link></li>
-                                    <li><Link className="dropdown-item" to="/recruit">Grouplancers</Link></li>
-                                    
-                                </ul>
-                            </li>
-                            <li className="nav-item mx-2">
-                                <Link className="nav-link" to="/chatbox">ChatBox</Link>
-                            </li>
-                            <li className="nav-item mx-2">
-                                <Link className="nav-link" to="/aboutus">About Us</Link>
-                            </li>
-                        </ul>
-                        {
-                            !currentUser
-                                ? <>
-                                    <Link to="/signup"><button className="btn btn-outline-success mx-2" type="submit">Signup</button></Link>
-                                    <Link to="/login"><button className="btn btn-outline-success mx-2" type="submit">Login</button></Link>
-                                </>
-                                : null
-                        }
-                    </div>
-                </div>
-            </nav> */}
-           
-            <Router>
-                <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-                    <Container>
-                        <Navbar.Brand href="/">
-                            <img src="./grouplan.png" alt="Logo" style={{ width: "300px", height: "200px", justifyContent: "space-between", marginBottom: "-50px", marginTop: "-50px" }} />
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav">
-                            <span className="navbar-toggler-icon"></span>
-                        </Navbar.Toggle>
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="ms-auto">
-                                <Nav.Link href="/" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                                <Nav.Link href="sidebar" className={activeLink === 'groups' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('groups')}>Groups</Nav.Link>
-                                <Nav.Link href="liveside" className={activeLink === 'categories' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('categories')}>Categories</Nav.Link>
-                                <Nav.Link href="sidebar_ranking" className={activeLink === 'rankings' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('rankings')}>Rankings</Nav.Link>
-                                <Nav.Link href="chatbox" className={activeLink === 'chatbox' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('chatbox')}>ChatBox</Nav.Link>
-                                <Nav.Link href="aboutus" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>About Us</Nav.Link>
-                            </Nav>
-                            <span className="navbar-text">
-                                {/* <div className="social-icon">
-                                    <a href="#"><img src={navIcon1} alt="" /></a>
-                                    <a href="#"><img src={navIcon2} alt="" /></a>
-                                    <a href="#"><img src={navIcon3} alt="" /></a>
-                                </div> */}
-                                {
-                                    currentUser ?
-
-                                            <Nav.Link className="profile" href="userAccount"><button className="vvd"><span>Profile</span></button></Nav.Link>
-                                             : <HashLink to='#connect'>
-                                            <button className="vvd"><span>Sign Up/Sign In</span></button>
-                                        </HashLink>
-                                }
-                            </span>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </Router>
-           
-
-        </>
-    )
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link href="/" style={{ color: "black", textDecoration: "none" }}>Home</Link>
+                </Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link to="/sidebar" style={{ color: "black", textDecoration: "none" }}>My Portfolios</Link>
+                </Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link href="/sidebar" style={{ color: "black", textDecoration: "none" }}>Chat</Link>
+                </Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link href="/createport" style={{ color: "black", textDecoration: "none" }}>Build your Site</Link>
+                </Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              <Link href="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              <Link href="/sidebar" style={{ color: "white", textDecoration: "none" }}>My Portfolios</Link>
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              <Link href="/about" style={{ color: "white", textDecoration: "none" }}>Chat</Link>
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              <Link href="/createport" style={{ color: "white", textDecoration: "none" }}>Build your Site</Link>
+            </Button>
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
-export default NavBar
+export default NavBar;
