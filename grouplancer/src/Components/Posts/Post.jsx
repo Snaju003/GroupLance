5a5ca9c0c3912b435c59c10aaf4e9b1dd0b03aef
Romit2004 <romitguha7@gmail.com>
2024-02-27 21:58:26 +0530

    modified:   grouplancer/package-lock.json 	modified:   grouplancer/package.json 	modified:   grouplancer/public/index.html 	modified:   grouplancer/src/Components/Posts/Post.jsx 	modified:   grouplancer/src/Components/Posts/Post_css.css

--- grouplancer/src/Components/Posts/Post.jsx
+++ grouplancer/src/Components/Posts/Post.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Post_css.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Post = () => {
     const [likes, setLikes] = useState(0);
     const [comments, setComments] = useState([]);
@@ -18,35 +20,55 @@ const Post = () => {
     };
 
     return (
-        <div className="post-container">
+        <div className="post-container" style={{display: "flex",flexDirection:"column"}}>
             <div style={{ display: "flex" }}>
                 <div>
-                    <img src='./aboutUs.jpg' alt='posts' style={{
+                    <img src='./creategrp.jpg' alt='posts' style={{
                         marginRight: "20px",
-                        marginLeft: "20px",
-                        height: "200px",
-                        width: "200px",
+                        marginLeft: "60px",
+                        height: "300px",
+                        width: "300px",
                         marginTop: "10px",
+                        borderRadius: "0px"
+                        
                     }} />
-                    <button className="like-button" onClick={handleLike}>
-                        Like ({likes})
+                  
+               
+                <div className="comment-section " style={{display:"flex", flexDirection:"row"}}>
+                <div style={{display: "flex", justifyContent: "space-between"}}>
+                <button className="like-button" onClick={handleLike} style={{marginTop:"15.008px", paddingRight:"2rem"}}>
+                <FontAwesomeIcon icon="fa-solid fa-thumbs-up" />
+                Like
+                ({likes})
                     </button>
-                </div>
-                <div className="comment-section" style={{ borderLeft: "3px solid white"}}>
                     <div style={{display: "flex"}}>
-                        <input
+                    
+                        <button className="comment-button" onClick={handleComment} style={{marginLeft:"1.5rem"}}>
+                            Comment
+                        </button>
+                        <button className="comment-button"  style={{marginLeft:"1.5rem"}}>
+                            Share
+                        </button>
+
+                      
+                </div>
+                
+                    </div>
+                    <div>
+                    
+                    
+                    </div>
+                </div>
+                </div>
+            </div>
+            <input class="input"
                             type="text"
                             placeholder="Add a comment"
                             value={commentInput}
                             onChange={(e) => setCommentInput(e.target.value)}
                             className="comment-input"
                         />
-                        <button className="comment-button" onClick={handleComment}>
-                            Comment
-                        </button>
-                    </div>
-                    <div>
-                        <h4 style={{ marginTop: "10px", color: "white", marginLeft: "2vh", }}>Comments:</h4>
+            <h4 style={{ marginTop: "10px", color: "white", marginLeft: "2vh", }}>Comments:</h4>
                         <ul className="comments-list">
                             {comments.map((comment, index) => (
                                 <li key={index} className="comment-item" style={{ color: "white" }}>
@@ -54,9 +76,6 @@ const Post = () => {
                                 </li>
                             ))}
                         </ul>
-                    </div>
-                </div>
-            </div>
         </div>
     );
 };
