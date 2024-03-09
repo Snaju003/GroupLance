1a49bdd46a15ab42db3aa25f36aa461e446128fb
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-03-10 01:03:05 +0530

    modified:   grouplancer/src/App.jsx 	new file:   grouplancer/src/Components/Posts/Mypost.jsx 	new file:   grouplancer/src/Components/Posts/Myposts.jsx 	modified:   grouplancer/src/Components/Posts/Post.jsx 	modified:   grouplancer/src/Components/Posts/PostBar.jsx

---
+++ grouplancer/src/Components/Posts/Myposts.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Mypost from './Mypost';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
+import Post from './Post';
+function Myposts() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const title = "kalo Romit";
+  const description = "he root layout is defined at the top level of the app directory and applies to all routes. This layout is required and must contain html and body tags, allowing you to modify the initial HTML returned from the server.";
+  const groupImage = "/creategrp.jpg";
+  const color = "#dfdffb";
+
+  const handleContainerClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <h1 className='text-center my-4' style={{ color: '#ffff', fontWeight: "bold" }}>My Posts</h1>
+      <div className="container">
+        <div className="container row" style={{ flexDirection: "column" }}>
+          <div class="col-md-3 mb-3" style={{ width: "100%", height: "100%" }} onClick={handleContainerClick}>
+            <Mypost groupName={title} postdesc={description} groupImage={groupImage} color={color} />
+          </div>
+        </div>
+      </div>
+      <Dialog open={isModalOpen} onClose={handleCloseModal}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          <Post groupName={title} postDescription={description} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+export default Myposts;
