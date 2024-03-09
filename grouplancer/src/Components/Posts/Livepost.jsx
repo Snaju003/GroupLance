1a9ef944c85e2803f27ba6d414135e322332f414
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-03-09 22:07:13 +0530

    modified:   grouplancer/src/Components/Posts/Livepost.jsx

--- grouplancer/src/Components/Posts/Livepost.jsx
+++ grouplancer/src/Components/Posts/Livepost.jsx
@@ -6,27 +6,28 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 
 import { Typography, IconButton, Box } from "@mui/material";
-//import Typography from "@mui/material/Typography";
 import StarIcon from "@mui/icons-material/Star";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
 function Livepost(props) {
   const theme = useTheme();
   const { color, groupName, groupImage, postdesc } = props;
   const [rating, setRating] = useState(0);
+
   const handleRating = (starCount) => {
     setRating(starCount);
   };
+
   return (
     <>
       <Card sx={{ display: "flex", maxWidth: "100%" }}>
-        <CardMedia
-          component="img"
-          sx={{ margin: "5rem", width: "25%", height: "50%" ,borderRadius:"0"}}
-          image={groupImage}
-          alt="Group Image"
-        />
+        {groupImage && groupImage.trim() !== "" && (
+          <CardMedia
+            component="img"
+            sx={{ margin: "5rem", width: "25%", height: "50%", borderRadius: "0" }}
+            image={groupImage}
+            alt="Group Image"
+          />
+        )}
         <Box sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
           <CardContent sx={{ flex: "1 0 auto" }}>
             <Typography component="div" variant="h5">
@@ -39,24 +40,21 @@ function Livepost(props) {
             >
               {postdesc}
             </Typography>
-          
-          
           </CardContent>
-            <Box display="flex" alignItems="center" marginBottom={15} >
-              {[1, 2, 3, 4, 5].map((star) => (
-                <IconButton
-                  key={star}
-                  onClick={() => handleRating(star)}
-                  color={star <= rating ? "warning" : "inherit"}
-                >
-                  <StarIcon />
-                </IconButton>
-              ))}
-              <Typography variant="body1" style={{ color: "black" }}>
-                Rated: {rating} stars
-              </Typography>
-              
-            </Box>
+          <Box display="flex" alignItems="center" marginBottom={15} >
+            {[1, 2, 3, 4, 5].map((star) => (
+              <IconButton
+                key={star}
+                onClick={() => handleRating(star)}
+                color={star <= rating ? "warning" : "inherit"}
+              >
+                <StarIcon />
+              </IconButton>
+            ))}
+            <Typography variant="body1" style={{ color: "black" }}>
+              Rated: {rating} stars
+            </Typography>
+          </Box>
         </Box>
       </Card>
     </>
