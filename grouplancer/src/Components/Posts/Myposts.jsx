ed42bb7407927877a266dd9d4159a91b46f7ae0e
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-03-10 02:55:10 +0530

    modified:   grouplancer/src/Components/Posts/Myposts.jsx

--- grouplancer/src/Components/Posts/Myposts.jsx
+++ grouplancer/src/Components/Posts/Myposts.jsx
@@ -1,16 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import Mypost from './Mypost';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
-import Post from './Post';
+import React, { useEffect, useState } from "react";
+import Mypost from "./Mypost";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+} from "@mui/material";
+import Post from "./Post";
 
 function Myposts() {
   const title = "kalo Romit";
-  const description = "he root layout is defined at the top level of the app directory and applies to all routes. This layout is required and must contain html and body tags, allowing you to modify the initial HTML returned from the server.";
+  const description =
+    "he root layout is defined at the top level of the app directory and applies to all routes. This layout is required and must contain html and body tags, allowing you to modify the initial HTML returned from the server.";
   const groupImage = "/creategrp.jpg";
   const color = "#dfdffb";
-  const [posts, setPosts] = useState()
+  const [posts, setPosts] = useState();
   const authToken = localStorage.getItem("auth-token");
-  
+
   useEffect(() => {
     const fetchPost = async () => {
       try {
@@ -25,28 +32,43 @@ function Myposts() {
           }
         );
         const data = await response.json();
-        setPosts(data.posts)
-        console.log(data)
+        setPosts(data.posts);
+        console.log(data);
       } catch (error) {
         console.error(error);
       }
-    }
-    fetchPost()
-  }
-  ,[authToken])
+    };
+    fetchPost();
+  }, [authToken]);
 
   return (
     <>
-      <h1 className='text-center my-4' style={{ color: '#ffff', fontWeight: "bold" }}>My Posts</h1>
+      <h1
+        className="text-center my-4"
+        style={{ color: "#ffff", fontWeight: "bold" }}
+      >
+        My Posts
+      </h1>
       <div className="container">
-        <div className="container row" style={{ flexDirection: "column", display: "flex", }}>
-          {
-            posts&&posts?.map(({groupId,content}) => {
-              <div class="col-md-3 mb-3" style={{ width: "100%", height: "100%" }} >
-                <Mypost groupName={groupId.gName} postdesc={content} groupImage={groupImage} color={color} />
+        <div
+          className="container row"
+          style={{ flexDirection: "column", display: "flex" }}
+        >
+          {posts &&
+            posts.map(({ groupId, content }) => (
+              <div
+                className="col-md-3 mb-3"
+                style={{ width: "100%", height: "100%" }}
+                key={groupId}
+              >
+                <Mypost
+                  groupName={groupId.gName}
+                  postdesc={content}
+                  groupImage={groupImage}
+                  color={color}
+                />
               </div>
-            })
-          }
+            ))}
         </div>
       </div>
       {/* <Dialog open={isModalOpen} onClose={handleCloseModal}>
