06520200de8f2308d2e4aff7c4874e7706553ca4
Subhadeep-coder <dharsubhadeep5@gmail.com>
2024-03-11 02:36:58 +0530

    modified:   Backend/Routes/tweet.js 	modified:   Backend/controllers/tweet.js

--- Backend/Routes/tweet.js
+++ Backend/Routes/tweet.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const fetchUser = require('../middleware/fetchuser');
-const { createTweet, deleteTweet, getAllTweets } = require('../controllers/tweet');
+const {
+    createTweet,
+    deleteTweet,
+    getAllTweetsBasedOnGroup,
+    getPosts
+} = require('../controllers/tweet');
 const upload = require("../database/fileUpload");
 
 const tweetRouter = express.Router();
 
 tweetRouter.post('/create-post', fetchUser, upload.single("file"), createTweet);
 tweetRouter.delete('/delete-post', fetchUser, deleteTweet);
-tweetRouter.get('/get-posts/:id', fetchUser, getAllTweets);
+tweetRouter.get('/get-posts/:id', fetchUser, getAllTweetsBasedOnGroup);
+tweetRouter.get('/get-all-posts',  getPosts);
 
 module.exports = tweetRouter;
