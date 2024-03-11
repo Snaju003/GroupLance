65d9824792b8a7f64719e88113ffdd4770540e86
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-03-12 01:24:55 +0530

    modified:   test.js

--- test.js
+++ test.js
@@ -1,51 +1,73 @@
-const stack = ["FIBO", "SQ_NO_SER", "PENT_SER", "MAG_SQ_SER", "LOOK_SAY_SEQ", "CAT_SER"];
+const stack = [
+  "FIBO",
+  "SQ_NO_SER",
+  "PENT_SER",
+  "MAG_SQ_SER",
+  "LOOK_SAY_SEQ",
+  "CAT_SER",
+];
 
 function stringToNum(str) {
-    let asciiArray = "";
-    for (let i = 0; i < str.length; i++) {
-        asciiArray += str.charCodeAt(i);
-    }
-    return asciiArray;
+  let asciiArray = "";
+  for (let i = 0; i < str.length; i++) {
+    asciiArray += str.charCodeAt(i);
+  }
+  return asciiArray;
 }
 
-function executeCommands(command, str) {
-    let result;
-    switch (command) {
-        case "FIBO":
-            result = genFibo(str);
-            break;
-        case "SQ_NO_SER":
-            break;
-        case "PENT_SER":
-            break;
-        case "MAG_SQ_SER":
-            break;
-        case "LOOK_SAY_SEQ":
-            break;
-        case "CAT_SER":
-            break;
-    }
-    return result;
+async function executeCommands(command, str) {
+  let result;
+  switch (command) {
+    case "FIBO":
+      result = await genFibo(str);
+      break;
+    case "SQ_NO_SER":
+      break;
+    case "PENT_SER":
+      break;
+    case "MAG_SQ_SER":
+      break;
+    case "LOOK_SAY_SEQ":
+      break;
+    case "CAT_SER":
+      break;
+  }
+  return result;
 }
 
 function fibo(x) {
-    
+  if (x === 0) {
+    return 0;
+  }
+  if (x === 1 || x === 2) {
+    return 1;
+  } else {
+    return fibo(x - 1) + fibo(x - 2);
+  }
 }
 
-function genFibo(str) {
-    let result;
-    for (let i = 0; i < str.length; i++) {
-        
-    }
+async function genFibo(str) {
+  let result;
+  for (let i = 0; i < str.length; i++) {
+    result += await fibo(i);
+  }
+  return result;
 }
 
 async function init(string) {
-    let str = stringToNum(string);
-    console.log(str);
-    for (let index = 0; index < stack.length; index++) {
-        str = executeCommands(stack[index], str);
-    }
-    return str;
+  let str = stringToNum(string);
+  console.log(str);
+  for (let index = 0; index < stack.length; index++) {
+    str = await executeCommands(stack[index], str);
+  }
+  return str;
 }
 
-console.log(init("Hello world"));
+async function main(){
+    const encoded =await init("Hello")
+    console.log(encoded);
+}
+
+main()
+
+
