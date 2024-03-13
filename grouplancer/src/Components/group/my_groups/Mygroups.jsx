e13aead80ef9189541bca7134a286bc8a95adbf7
PratyushPal29 <29palpratyush04@gmail.com>
2024-03-13 12:28:32 +0530

    modified:   grouplancer/src/Components/group/my_groups/Mygroups.jsx

--- grouplancer/src/Components/group/my_groups/Mygroups.jsx
+++ grouplancer/src/Components/group/my_groups/Mygroups.jsx
@@ -53,9 +53,8 @@ const MyGroups = () => {
                     }
                 </div>
             </div>
-            <div className="card" style={{ backgroundColor: color, width:"20rem" }}>
+            {/* <div className="card" style={{ backgroundColor: color, width:"20rem" }}>
                 <div className="card-body"style={{}}>
-                    {/* <h5 className="card-title">{name}</h5> */}
                     <p className="card-text" style={{fontWeight:"500px",fontSize:"2rem",marginLeft:"1rem"}}>Create a group</p>
                     <button className='button-48' style={{translate:"4rem"}}>
                     <Link to='/creategroup' style={{textDecoration :"none", color: "white", fontWeight: "700"}} >
@@ -65,7 +64,7 @@ const MyGroups = () => {
                     </button>
                     
                 </div>
-            </div >
+            </div > */}
         </>
     )
 }
