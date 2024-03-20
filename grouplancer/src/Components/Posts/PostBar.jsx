4a5e1a4793675f4ee818ff0bae7e463852aff0e9
PratyushPal29 <29palpratyush04@gmail.com>
2024-03-21 00:39:42 +0530

    modified:   grouplancer/src/Components/Posts/PostBar.jsx

--- grouplancer/src/Components/Posts/PostBar.jsx
+++ grouplancer/src/Components/Posts/PostBar.jsx
@@ -1,58 +1,38 @@
-import React, { useState } from "react";
-
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import CreatePost from "./CreatePost";
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
 import CreatePost1 from "./CreatePost1";
-import Post from "./Post";
-import LivePosts from "./LivePosts";
-import Mypost from "./Mypost";
 import Myposts from "./Myposts";
+import LivePosts from "./LivePosts";
 
 const Postbar = () => {
-
-    return (
-        <>
-            <section className="tab">
-                <Container>
-                    <Row>
-                        <Col size={12}>
-                            <Tab.Container id="tabs-group" defaultActiveKey="first">
-                                <Nav fill variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="first">Live Posts</Nav.Link>
-                                    </Nav.Item>
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="second">Create Post</Nav.Link>
-                                    </Nav.Item>
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="third">My Posts</Nav.Link>
-                                    </Nav.Item>
-                                    {/* <Nav.Item>
-                                        <Nav.Link eventKey="fourth">Group Invite</Nav.Link>
-                                    </Nav.Item> */}
-                                </Nav>
-                                <Tab.Content id="slideInUp">
-                                    <Tab.Pane eventKey="first">
-                                        <LivePosts />
-                                    </Tab.Pane>
-                                    <Tab.Pane eventKey="second">
-                                        <CreatePost1 />
-                                    </Tab.Pane>
-                                    <Tab.Pane eventKey="third">
-                                        <Myposts />
-                                    </Tab.Pane>
-                                    {/* <Tab.Pane eventKey="fourth">
-                                        <GroupsInvite />
-                                    </Tab.Pane> */}
-                                </Tab.Content>
-                            </Tab.Container>
-
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-        </>
-    );
-};
+  return (
+    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+      <Row>
+        <Col sm={3} style={{ marginTop: "20rem", marginLeft: "auto", fontSize: "20px", width: "22rem" }}>
+          <Nav variant="pills" className="flex-column">
+            <Nav.Item>
+              <Nav.Link className="text-white" style={{marginBottom: "1rem", borderRadius: "20px", textAlign: "center", backgroundColor: "#0e065a"}} eventKey="first">Live Posts</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link className="text-white" style={{marginBottom: "1rem", borderRadius: "20px", textAlign: "center", backgroundColor: "#0e065a"}} eventKey="second">Create Post</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link className="text-white" style={{marginBottom: "1rem", borderRadius: "20px", textAlign: "center", backgroundColor: "#0e065a"}} eventKey="third">My Posts</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col sm={9}>
+          <Tab.Content>
+            <Tab.Pane eventKey="first"><LivePosts /></Tab.Pane>
+            <Tab.Pane eventKey="second"><CreatePost1 /></Tab.Pane>
+            <Tab.Pane eventKey="third"><Myposts /></Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
+  );
+}
 
 export default Postbar;
