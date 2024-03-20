44aa8daf877f4e9444b11c24970694f60fdc2c63
PratyushPal29 <29palpratyush04@gmail.com>
2024-03-20 21:13:57 +0530

    modified:   grouplancer/src/Components/group/CreateGroup.jsx

--- grouplancer/src/Components/group/CreateGroup.jsx
+++ grouplancer/src/Components/group/CreateGroup.jsx
@@ -113,8 +113,8 @@ const CreateGroup = () => {
                 </Col>
               </form>
               <div className="boximage" style={{gap:"6rem"}}>
-              <img src="./creategrp.jpg" alt="goru" style={{ borderRadius:"30px 30px 0px 0px",height:"400px",width:"450px"}}/>
-              <img src="./creategrp3.avif" alt="goru" style={{ borderRadius:"0px 0px 30px 30px",height:"400px",width:"450px"}}/>
+              <img src="./creategrp.jpg" alt="group" style={{ borderRadius:"30px 30px 0px 0px",height:"400px",width:"450px"}}/>
+              <img src="./creategrp3.avif" alt="group" style={{ borderRadius:"0px 0px 30px 30px",height:"400px",width:"450px"}}/>
               </div>
             </Col>
           </Row>
