77ebb1735734b664382d70fca9de742b7281c3c6
PratyushPal29 <29palpratyush04@gmail.com>
2024-04-05 02:11:26 +0530

    modified:   grouplancer/src/Components/auth/user-accounts/UserAccounts.jsx 	modified:   grouplancer/src/Components/group/top_grouplancers/Recruit.jsx 	modified:   grouplancer/src/Components/group/top_grouplancers/Recruits.jsx

--- grouplancer/src/Components/group/top_grouplancers/Recruits.jsx
+++ grouplancer/src/Components/group/top_grouplancers/Recruits.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Recruit from "./Recruit";
+import { useUser } from "../../../context/UserContext";
 
 const Recruits = () => {
     const title = "hello";
     const description = "world";
     const color = "#dfdffb";
+    const { currentUser } = useUser();
+    const [users, setUsers] = useState()
+
+    useEffect(() => {
+        const getAllGrouplancers = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/api/user/get-all-users", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "auth-token": localStorage.getItem("auth-token"),
+                    },
+                });
+                const json = await response.json();
+                setUsers(json.users)
+                console.log(users)
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        getAllGrouplancers();
+    }, [currentUser])
+
     return (
         <>
             <h1 className='text-center my-4' style={{ color: '#ffff' }}>Top Grouplancers</h1>
             <div className="container">
-                <div className="container row" style={{flexDirection:"column"}}>
-                    <div class="col-md-3 mb-3">
-                        <Recruit title={title} description={description} color={color} />
-                    </div>
-                    <div class="col-md-3 mb-3">
-                        <Recruit title={title} description={description} color={color} />
-                    </div>
-                    
+                <div className="container row" style={{ flexDirection: "column" }}>
+                    {users && users.map(({ name, email, rate, profile_pic }) => {
+                        return (
+                            <div class="col-md-3 mb-3">
+                                <Recruit name={name} email={email} rate={rate} profilePic={profile_pic} color={color} />
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </>
