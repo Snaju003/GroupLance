d00f2a2a8859f4a687a66695a493b8cdadb93da7
PratyushPal29 <29palpratyush04@gmail.com>
2024-04-06 02:18:39 +0530

    modified:   grouplancer/src/Components/Posts/create-post/CreatePost1.jsx 	modified:   grouplancer/src/Components/Posts/my-posts/LivePosts.jsx

--- grouplancer/src/Components/Posts/create-post/CreatePost1.jsx
+++ grouplancer/src/Components/Posts/create-post/CreatePost1.jsx
@@ -19,11 +19,11 @@ function convertToBase64(file) {
 }
 
 const CreatePost1 = () => {
-
     const navigate = useNavigate();
     const [myGroups, setMyGroups] = useState([]);
     const { currentUser } = useUser();
     const [selGroup, setSelGroup] = useState();
+    const [img, setImg] = useState(null)
 
     const [formData, setFormData] = useState({
         pDesc: "",
@@ -31,26 +31,6 @@ const CreatePost1 = () => {
     });
 
     const [postImage, setPostImage] = useState("");
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const authToken = localStorage.getItem('auth-token');
-                const response = await fetch("http://localhost:8080/api/file-upload/get-user-picture", {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "auth-token": authToken,
-                    },
-                });
-                const data = await response.json();
-                // console.log(data.existsImage.image);
-                setFormData({ ...formData, media: data.existsImage.image });
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchData();
-    }, [])
 
     useEffect(() => {
         const fetchData = async () => {
@@ -77,14 +57,14 @@ const CreatePost1 = () => {
         try {
             e.preventDefault();
             const authToken = localStorage.getItem('auth-token');
-            console.log(selGroup)
+            console.log(img)
             const response = await fetch("http://localhost:8080/api/tweet/create-post", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                     "auth-token": authToken,
                 },
-                body: JSON.stringify({ groupId: selGroup, content: formData.pDesc })
+                body: JSON.stringify({ groupId: selGroup, content: formData.pDesc, file: img })
             });
             const data = await response.json();
             console.log(data);
@@ -102,26 +82,29 @@ const CreatePost1 = () => {
         const base64 = await convertToBase64(file);
         setPostImage(base64);
         if (!postImage) {
-            e.target.value = null;
-            return;
+          e.target.value = null;
+          return;
         }
         try {
-            e.preventDefault();
-            const authToken = localStorage.getItem('auth-token');
-            const response = await fetch("http://localhost:8080/api/file-upload/user", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "auth-token": authToken,
-                },
-                body: JSON.stringify({ file: postImage })
-            });
-            const data = await response.json();
-            console.log(data);
+          e.preventDefault();
+          const authToken = localStorage.getItem('auth-token');
+          const response = await fetch("http://localhost:8080/api/file/upload-post-img", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "auth-token": authToken,
+            },
+            body: JSON.stringify({ file: postImage })
+          });
+          const data = await response.json();
+          console.log(data.image._id);
+          setImg(data.image._id);
+          setFormData({ media: base64 });
+          console.log("Img: ",img);
         } catch (error) {
-            console.log(error)
+          console.log(error)
         }
-    }
+      }
 
     const handleChange = (e) => {
         if (e.target.name === "media") {
