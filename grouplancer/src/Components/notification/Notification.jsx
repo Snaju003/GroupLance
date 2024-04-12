9bb36b768540dbefbaf8289e0fdbde9c65dfd2b3
PratyushPal29 <29palpratyush04@gmail.com>
2024-04-13 02:35:53 +0530

    modified:   grouplancer/src/Components/notification/Notification.jsx 	modified:   grouplancer/src/Components/notification/invite.jsx

--- grouplancer/src/Components/notification/Notification.jsx
+++ grouplancer/src/Components/notification/Notification.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Invite from "./invite";
 import Layout from "../Layout/Layout";
 
@@ -6,20 +6,45 @@ const Notification = () => {
     const title = "hello";
     const description = "world";
     const color = "#dfdffb";
+    const [invite, setInvite] = useState([])
+
+    useEffect(() => {
+        const fetchInvite = async () => {
+            try {
+                const authToken = localStorage.getItem('auth-token');
+                const response = await fetch("http://localhost:8080/api/invite/get-all-invites", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "auth-token": authToken,
+                    },
+                });
+                const data = await response.json();
+                console.log(data.invites);
+                setInvite(data.invites)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchInvite();
+    }
+        , [])
+
     return (
         <>
-        <Layout>
-            <h1 className='text-center my-4' style={{ color: '#ffff' }}>Notifications</h1>
-            <div className="container">
-                <div className="container row" style={{flexDirection:"column"}}>
-                    <div class="col-md-3 mb-3">
-                        <Invite title={title} description={description} color={color} />
-                    </div>
-                    <div class="col-md-3 mb-3">
-                        <Invite title={title} description={description} color={color} />
+            <Layout>
+                <h1 className='text-center my-4' style={{ color: '#ffff' }}>Notifications</h1>
+                <div className="container">
+                    <div className="container row" style={{ flexDirection: "column" }}>
+                        {invite && invite.map(() => {
+                            return (
+                                <div class="col-md-3 mb-3">
+                                    <Invite title={title} description={description} color={color} />
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
-            </div>
             </Layout>
         </>
     )
--- grouplancer/src/Components/notification/invite.jsx
+++ grouplancer/src/Components/notification/invite.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useUser } from "../../context/UserContext"
 
 const Invite = (props) => {
     let { title, description, color } = props;
+    const { currentUser } = useUser();
 
     // const addMember = async (e) => {
     //     e.preventDefault();
@@ -17,6 +19,37 @@ const Invite = (props) => {
     //     console.log(json)
     // }
 
+    // const acceptInvite = async (e) => {
+    //     e.preventDefault();
+    //     const response = await fetch("http://localhost:8080/api/group/join-group", {
+    //         method: "PUT",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "auth-token": localStorage.getItem('auth-token'),
+    //         },
+    //         body: JSON.stringify({ groupId: id }),
+    //     });
+    //     const json = await response.json();
+    //     console.log(json)
+    // }
+
+    const rejectInvite = async () => {
+        try {
+            const authToken = localStorage.getItem('auth-token');
+            const response = await fetch(`http://localhost:8080/api/invite//reject-invite/${currentUser._id}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "auth-token": authToken,
+                },
+            });
+            const json = await response.json();
+            console.log(json);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className="card" style={{ backgroundColor: color, width: "80vw" }}>
@@ -27,7 +60,7 @@ const Invite = (props) => {
                     <p className="card-text">{description}</p>
                     <div atyle={{ display: "flex", juatifyContent: "space-between" }}>
                         <a href="/" target='__blank' className="btn btn-primary" style={{ marginRight: "1rem" }}>Accept</a>
-                        <a href="/" target='__blank' className="btn btn-primary">Reject</a>
+                        <a href="/" target='__blank' className="btn btn-primary" onClick={rejectInvite}>Reject</a>
                     </div>
 
                 </div>
